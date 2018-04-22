b7393d2f348742feeb7f096d1480ab5ab2b32f53
Srinivasan Balaji <Srinivasan_Balaji@symantec.com>
2018-04-22 15:10:49 +0530

    Initial Structure

---
+++ Project_Euler_Solution/Project_Euler_Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Euler_Solution
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
